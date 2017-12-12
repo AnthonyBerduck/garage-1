5a1084fda280c268d445e2b9611a0a8f6e9c4cb6
Charles Sayamath <charles.sayamath@gmail.com>
2017-12-12 16:07:52 +0100

    controleurCharles

--- controleur/controleur.php
+++ controleur/controleur.php
@@ -1,3 +1,18 @@
 <?php
   require_once("modele/modele.php");
   require_once("vue/vue.php");
+
+  function ctlAjouterClient($nom,$prenom,$adresse,$numTel,$mail,$montantMax){
+    if(!empty($nom) && !empty($prenom) && !empty($adresse) && !empty($numTel) && !empty($mail) && !empty($montantMax)) && (strlen((string)$numTel))==10){
+      ajouterClient($nom,$prenom,$adresse,$numTel,$mail,$montantMax);
+    }
+    else {
+      throw new Exception("Un ou plusieurs champs sont invalides");
+    }
+  }
+
+  function ctlSAjouterEmploye($)
+
+  function ctlErreur($erreur){
+    afficherErreur($erreur);
+  }
